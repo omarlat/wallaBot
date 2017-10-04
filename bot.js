3d6a11108d06b130ec3c30e11ab3c686096b636f
Unknown <omartinezlatasa@gmail.com>
2017-10-04 21:43:59 +0200

    Unificando el listener y el scraper

--- bot.js
+++ bot.js
@@ -1,7 +1,8 @@
 var TelegramBot = require('node-telegram-bot-api');
 var telegramToken = process.env.TG_TOKEN;
 var telegramChatId = process.env.TG_CHAT_ID;
-var bot = new TelegramBot(telegramToken, {polling: false});
+var bot = new TelegramBot(telegramToken, {polling: true});
+var wallapopModel = require('./models/wallapopModel.js');
 
 var myBot = {};
 
@@ -21,5 +22,21 @@ myBot.sendMessages = async function (messages){
     bot.sendMessage(telegramChatId, messages[i], {parse_mode: "html"});
   }
 }
+
+bot.on('text', function (msg) {
+  var chatId = msg.chat.id;
+  var text = msg.text.toLowerCase();
+  if(chatId ==telegramChatId){
+    if(text.startsWith("add")){
+      var kws = text.substr(4);
+      wallapopModel.insertSearch(kws);
+      bot.sendMessage(chatId,'Busqueda de '+kws+' añadida');
+    }else{
+      bot.sendMessage(chatId,"No entiendo tu orden");
+    }
+  }
+});
+
+
 
 module.exports = myBot;
