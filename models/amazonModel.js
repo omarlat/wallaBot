f3aecb1113da160742c2f764c9132a14ea11ccd8
Arkaitz Carbajo <arkaitz.carbajo@gmail.com>
2017-11-22 00:00:29 +0100

    promises added

--- models/amazonModel.js
+++ models/amazonModel.js
@@ -16,142 +16,122 @@ var amazonModel = {};
 
 
 amazonModel.getItem = async function (item) {
-    try {
-        //Find item
-        let item = await db.users.find({
+    return new Promise(function (resolve, reject) {
+        db.items.find({
             id: item
         }, function (err, docs) {
-            if (docs.length > 0) {
-                return docs[0];
+            if (err) {
+                reject(err);
             } else {
-                console.log("No results found");
-                return {};
+                resolve(docs);
             }
-        });
-
-        return item;
-    } catch (error) {
-        console.log(error);
-        //ctx.throw(400, 'INVALID_DATA');
-    }
+        })
+    });
 }
 
 amazonModel.getSearchs = async function () {
-    try {
-        //Find item
-
-        let searches = await db.searches.find({}, function (err, docs) {
-            return docs;
-        });
-
-        return searches;
-
-    } catch (error) {
-        console.log(error);
-        //ctx.throw(400, 'INVALID_DATA');
-    }
+    return new Promise(function (resolve, reject) {
+        db.searches.find({}, function (err, docs) {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(docs);
+            }
+        })
+    });
 }
 
 amazonModel.getItems = async function (chatId) {
-    try {
-        //Find item
-
-        let items = await db.items.find({}, function (err, docs) {
-            return docs;
-        });
-
-        return items;
-
-    } catch (error) {
-        console.log(error);
-        //ctx.throw(400, 'INVALID_DATA');
-    }
+    return new Promise(function (resolve, reject) {
+        db.items.find({}, function (err, docs) {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(docs);
+            }
+        })
+    });
 }
 
 amazonModel.updateSearch = async function (search) {
-    try {
-
-        await db.searches.update({
+    return new Promise(function (resolve, reject) {
+        db.searches.update({
             id: search
+        }, function (err, numReplaced) {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(numReplaced);
+            }
+        })
+    });
+}
+
+amazonModel.insertItem = async function (id, item_id, chat_id, price, title) {
+    var item = {
+        id: id,
+        item_id: item_id,
+        chat_id: chat_id,
+        price: price.replace(',', '.'),
+        title: title
+    };
+    
+    return new Promise(function (resolve, reject) {
+        db.items.insert(item, function (err, newDoc) {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(newDoc);
+            }
+        })
+    });
+}
+
+amazonModel.updateItem = async function (id, price) {
+    return new Promise(function (resolve, reject) {
+        db.items.update({
+            id: id
         }, {
-            last: new Date()
+            price: price
         }, {}, function (err, numReplaced) {
-            console.log("registros actualizados: " + numReplaced);
-        });
-
-    } catch (error) {
-        console.log(error);
-        //ctx.throw(400, 'INVALID_DATA');
-    }
+            if (err) {
+                reject(err);
+            } else {
+                resolve(numReplaced);
+            }
+        })
+    });
 }
 
-amazonModel.insertItem = async function (_id, _item_id, _chat_id, _price, _title) {
-    try {
-        var item = {
-            id: _id,
-            item_id: _item_id,
-            chat_id: _chat_id,
-            price: _price.replace(',', '.'),
-            title: _title
-        };
-        await db.items.insert(item, function (err, newDoc) {
-            console.log("New item inserted: " + JSON.stringify(item));
-        });
-
-    } catch (error) {
-        console.log(error);
-        //ctx.throw(400, 'INVALID_DATA');
-    }
+amazonModel.insertSearch = async function (item_id, chat_id) {
+    var search = {
+        item_id: item_id,
+        chat_id: chat_id
+    };
+    return new Promise(function (resolve, reject) {
+        db.searches.insert(search, function (err, newDoc) {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(newDoc);
+            }
+        })
+    });
 }
 
-amazonModel.updateItem = async function (_id, _price) {
-    try {
-
-        await db.items.update({
-            id: _id
-        }, {
-            price: _price
-        }, {}, function (err, numReplaced) {
-            console.log("registros actualizados: " + numReplaced);
-        });
-
-    } catch (error) {
-        console.log(error);
-        //ctx.throw(400, 'INVALID_DATA');
-    }
-}
-
-amazonModel.insertSearch = async function (_item_id, _chat_id) {
-    try {
-
-        var search = {
-            _item_id: _item_id,
-            _chat_id: _chat_id
-        };
-        await db.searches.insert(search, function (err, newDoc) {
-            console.log("New search inserted: " + JSON.stringify(search));
-        });
-
-    } catch (error) {
-        console.log(error);
-        //ctx.throw(400, 'INVALID_DATA');
-    }
-}
-
-amazonModel.deleteSearch = async function (_item_id, _chat_id) {
-    try {
-
-        await db.searches.remove({
-            item_id: _item_id,
-            chat_id: _chat_id
+amazonModel.deleteSearch = async function (item_id, chat_id) {
+    return new Promise(function (resolve, reject) {
+        db.searches.remove({
+            item_id: item_id,
+            chat_id: chat_id
         }, {}, function (err, numRemoved) {
-            console.log("Searches removed: " + numRemoved);
-        });
-
-    } catch (error) {
-        console.log(error);
-        //ctx.throw(400, 'INVALID_DATA');
-    }
+            if (err) {
+                reject(err);
+            } else {
+                resolve(numRemoved);
+            }
+        })
+    });
 }
 
 module.exports = amazonModel;
--- scrapers/amazonScraper.js
+++ scrapers/amazonScraper.js
@@ -3,57 +3,63 @@ var cheerio = require('cheerio');
 var amazonModel = require('../models/amazonModel.js');
 var amazonScraper = {};
 
-amazonScraper.execute = async function(cb){
+amazonScraper.execute = async function (cb) {
 
-  searchs  = await amazonModel.getSearchs();
-  search = searchs[0];
-  var options = {
-    host: 'www.amazon.es',
-    port: 443,
-    path: '/gp/product/'+(search.ITEM_ID),
-    method: 'GET'
-  }
-  console.log(options.path);
-  https.request(options, function(res){
-    console.log("status code: ", res.statusCode);
-    var body = '';
-    res.on('data', function(chunk){
-      body+=chunk;
-    });
+  searchs = await amazonModel.getSearchs();
 
-    res.on('end', async function(){
-      var $ = cheerio.load(body);
-      var price = "";
-      var title = $("title").text()
-      $('.a-span12 span.a-size-medium.a-color-price').each(function(i, element) {
-          var el = $(this);
-          price = el.text().replace('EUR ','').replace(',','.');
-          console.log(price);
-      })
-      var messages = [];
-      if(price!=""){
-          item  = await amazonModel.getItem(search.ID);
-          if(item=== undefined){
-              await amazonModel.insertItem(search.ID, search.ITEM_ID, search.CHAT_ID, price, title);
-              messages.push('<a href="https://www.amazon.es/gp/product/'+search.ITEM_ID+'">PRECIO ACTUAL DE '+title+': '+price+'€</a>');
-          }else if(item.PRICE>price){
-              await amazonModel.updateItem(search.ID, price);
-              messages.push('<a href="https://www.amazon.es/gp/product/'+search.ITEM_ID+'">BAJADA DE PRECIO DE '+title+' de '+item.PRICE+'€ a '+price+'€</a>');
-          }else if(item.PRICE<price){
-              await amazonModel.updateItem(search.ID, price);
-              messages.push('<a href="https://www.amazon.es/gp/product/'+search.ITEM_ID+'">SUBIDA DE PRECIO DE '+title+' de '+item.PRICE+'€ a '+price+'€</a>');
+  console.log("searches data: " + JSON.stringify(searchs));
+
+  if (searchs && searchs.length > 0) {
+    search = searchs[0];
+    var options = {
+      host: 'www.amazon.es',
+      port: 443,
+      path: '/gp/product/' + (search.item_id),
+      method: 'GET'
+    }
+    console.log(options.path);
+    https.request(options, function (res) {
+        console.log("status code: ", res.statusCode);
+        var body = '';
+        res.on('data', function (chunk) {
+          body += chunk;
+        });
+
+        res.on('end', async function () {
+          var $ = cheerio.load(body);
+          var price = "";
+          var title = $("title").text()
+          $('.a-span12 span.a-size-medium.a-color-price').each(function (i, element) {
+            var el = $(this);
+            price = el.text().replace('EUR ', '').replace(',', '.');
+            console.log(price);
+          })
+          var messages = [];
+          if (price != "") {
+            item = await amazonModel.getItem(search.id);
+            if (item === undefined) {
+              await amazonModel.insertItem(search.id, search.item_id, search.chat_id, price, title);
+              messages.push('<a href="https://www.amazon.es/gp/product/' + search.item_id + '">PRECIO ACTUAL DE ' + title + ': ' + price + '€</a>');
+            } else if (item.price > price) {
+              await amazonModel.updateItem(search.id, price);
+              messages.push('<a href="https://www.amazon.es/gp/product/' + search.item_id + '">BAJADA DE PRECIO DE ' + title + ' de ' + item.price + '€ a ' + price + '€</a>');
+            } else if (item.price < price) {
+              await amazonModel.updateItem(search.id, price);
+              messages.push('<a href="https://www.amazon.es/gp/product/' + search.item_id + '">SUBIDA DE PRECIO DE ' + title + ' de ' + item.price + '€ a ' + price + '€</a>');
+            }
+          } else {
+            console.log('Sin resultados de precio');
           }
-      }else{
-        console.log('Sin resultados de precio');
-      }
-      await amazonModel.updateSearch(search.ID);
-      cb(null, messages, search.CHAT_ID);
-    });
+          await amazonModel.updateSearch(search.id);
+          cb(null, messages, search.chat_id);
+        });
 
-    res.on('error', cb);
-  })
-  .on('error', cb)
-  .end();
+        res.on('error', cb);
+      })
+      .on('error', cb)
+      .end();
+  }
+
 }
 
 module.exports = amazonScraper;
